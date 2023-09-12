a384527750ddeeeeef10d8548e8eec40fe0aac78
Sam <sam+bsp@pxp.nz>
2023-09-12 23:01:12 +1200

    mongo connection working

--- backend/models/point.js
+++ backend/models/point.js
@@ -1,8 +1,13 @@
+// point.js
+
 const mongoose = require('mongoose');
 
 const pointSchema = new mongoose.Schema({
-  pointResult: String,
-  pointScore: String,
+  pointScore: String,                  // the score before the current point was resolved
+  wonByPlayer,                         // who won the point, player or opponent
+  pointResolution,                     // was the point an error or a winner
+  pointPlayedFromCourtPosition,        // where on the court wsa the error or winner played from
+  ballPlayedToPosition                 // where on the court did the error or winner finish
   game: { type: mongoose.Schema.Types.ObjectId, ref: 'Game' },
 });
 
